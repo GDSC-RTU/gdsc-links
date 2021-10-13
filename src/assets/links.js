27d6de20ca15979a3d9b711d3861129f0841ca6c
ishubham21 <sg2199203@gmail.com>
2021-10-13 18:44:00 +0530

    added data

---
+++ src/assets/links.js
@@ -0,0 +1,32 @@
+const links = [
+    {
+        name: "Become a Member",
+        link: "https://gdsc.community.dev/rajasthan-technical-university-kota/"
+    },
+    {
+        name: "Github",
+        link: "https://github.com/GDSC-RTU"
+    },
+    {
+        name: "LinkedIn",
+        link: "https://linkedin.com/company/dsc-rtu"
+    },
+    {
+        name: "YouTube",
+        link: "https://www.youtube.com/channel/UC4HOuA2c08DZU12SIRGPgvg"
+    },
+    {
+        name: "Twitter",
+        link: "https://twitter.com/dsc-rtu"
+    },
+    {
+        name: "Discord",
+        link: "https://discord.gg/u8mkFY82dA"
+    },
+    {
+        name: "E-mail",
+        link: "dsc.rtukota@gmail.com"
+    }
+]
+
+export default links

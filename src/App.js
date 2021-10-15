c95e7a54c7966a6419cd77903381b5d4640226c6
AniruddhaSoni <anirudh.soni007@gmail.com>
2021-10-15 11:12:31 +0530

    changelog added some icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import './App.css';
+import Link from "./components/link";
+import links from "./assets/links";
+import "./css/app.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <div className="intro">
+        <img
+          src="./assets/avatar.jpg"
+          alt=""
+          srcset=""
+          className="intro_avatar"
+        />
+        <div className="intro_name">Hi there, We're Google Developers Club</div>
+        <div className="intro_description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+          vitae dolor recusandae laboriosam aliquam, magni, non fuga cupiditate
+          totam voluptatibus consequatur! Dolor quisquam dolorem, ipsa ex
+          assumenda deleniti maiores beatae?
+        </div>
+      </div>
+      <div className="links">
+        {links.map((link) => {
+          return (
+            <Link
+              link={link.link}
+              name={link.name}
+              icon={`./assets/icons/${link.icon}`}
+            ></Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
